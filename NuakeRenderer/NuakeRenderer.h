0d94da474cb4807f77291bb2b2fac5e927582108
a.pilote <antoinepilote1@hotmail.com>
2022-03-26 23:41:54 -0400

    added comments

--- NuakeRenderer/NuakeRenderer.h
+++ NuakeRenderer/NuakeRenderer.h
@@ -1,18 +1,23 @@
+// Nuake Renderer is a helper library that with everything related to OpenGL rendering.
+// It is made to be low level and isn't trying to be a renderer that is already built for you.
 #include <string>
 #include "imgui.h"
 
 namespace NuakeRenderer
 {
-	void SetClearColor(float r, float g, float b, float a);
-	void Clear();
-	void DrawElements();
-	void EnableGLDebug(bool value);
-
 	int Init();
-	void PollEvents();
-
 	std::string GetVersion();
 
+	// Enables debugging output
+	void EnableGLDebug(bool value);
+
+	void SetClearColor(float r, float g, float b, float a);
+	void Clear();
+
+	// Poll window events
+	void PollEvents();
+
+	// ImGui stuff
 	void ApplyNuakeImGuiTheme();
 	void BeginImGuiFrame();
 	void EndImGuiFrame();
